5b741878fda8613ce8004c55658140bb3f6bcc0b
Genti <gentius2@gmail.com>
2017-10-02 18:48:38 -0400

    reading char arrays within cells

--- src/main/java/wasdev/sample/rest/MetadataAPI.java
+++ src/main/java/wasdev/sample/rest/MetadataAPI.java
@@ -13,19 +13,30 @@ public class MetadataAPI {
 		// TODO Auto-generated method stub
 		
 		MatFileReader matfilereader = new MatFileReader("src/main/metadata/cars_meta.mat");
-		Map<String, MLArray> mp = matfilereader.getContent();
-		Iterator<?> it = mp.entrySet().iterator();
-		while (it.hasNext()) {
-			Map.Entry pair = (Map.Entry)it.next();
-	        System.out.println(pair.getKey() + " = " + pair.getValue());
-	        //it.remove(); // avoids a ConcurrentModificationException
-	    }
+		//Map<String, MLArray> mp = matfilereader.getContent();
+		//Iterator<?> it = mp.entrySet().iterator();
+		//while (it.hasNext()) {
+		//	Map.Entry pair = (Map.Entry)it.next();
+	    //    System.out.println(pair.getKey() + " = " + pair.getValue());
+	    //    //it.remove(); // avoids a ConcurrentModificationException
+	    //}
 		//MLArray mlArrayRetrieved = matfilereader.getMLArray("cell");
-		MLArray mlArray =  matfilereader.getMLArray("class_names");
-		System.out.println(mlArray.getSize());
-	    System.out.println(mlArray.contentToString());
+		MLCell mlArray =  (MLCell)  matfilereader.getMLArray("class_names");
+		System.out.println(mlArray.cells());
+		
+		for (int i = 0; i < mlArray.getSize(); i++){
+			MLChar mlChar = (MLChar) mlArray.get(i);
+			System.out.println(i + " " + mlChar.contentToString());
+			//System.out.print(mlArray.get(i).name); 
+		}
+		//byte[] arr = mlArray.getNameToByteArray();
+		//System.out.println(arr[0]);
+		//System.out.println();
+		//System.out.println(mlArray.getClass());
+		//System.out.println(((MLCell) mlArray).getIndex(2, 6));
+		
 	    //System.out.println(mlArrayRetrieved.contentToString());
-		//System.out.println(it.next());
+		//System.out.println(mlChar);
 		//System.out.println(st.getDescription());
 		//System.out.println(al.indexOf(0));
 	    //MLArray mlArrayRetrieved = matfilereader.getMLArray("data");
